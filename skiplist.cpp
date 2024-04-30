2da465af915f89cf279ff56121668f08619b9926
manojreddy06 <167643266+manojreddy06@users.noreply.github.com>
2024-04-30 18:39:02 +0530

    constructor code generated

--- skiplist.cpp
+++ skiplist.cpp
@@ -10,3 +10,45 @@ struct SkipNode
     int key;
     SkipNode* forword[MAX_LEVEL + 1];
 };
+
+class SkipList
+{
+ public:
+    Skiplist();
+    void insertElement(int key);
+    bool searchElement(int key);
+    void deleteElement(int key);
+    void displayList();
+
+ private:
+    int randomLevel();
+    SkipNode* createNode(int key, int level);
+    SkipNode* header;
+    int level;
+}
+//Constructor
+SkipList::SkipList()
+{
+    header = new SkipNode;
+    header->key = INI_MAX;
+    for (int i = 0; i <= MAX_LEVEL; i++)
+        header->forword[i] = nullptr;
+    level = 0;
+}
+
+int SkipList::randomLevel()
+{
+    int lvl = 0;
+    while (rand() % 2 && lvl <bMAX_LEVEL)
+        lvl++;
+    return lvl;
+}
+
+SkipNode* SkipList::createNode(int key, int level)
+{
+    SkipNode* newNode = new SkipNode;
+    newNode->key = key;
+    for (int i = 0; i<= level; i++)
+        newNode->forward[i] = nullptr;
+    return newNode;
+}
