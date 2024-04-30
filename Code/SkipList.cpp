b005b5e1e6d89ac444b8adab4ddc9017dc62be70
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2024-04-30 22:32:34 +0530

    Update SkipList.cpp

--- Code/SkipList.cpp
+++ Code/SkipList.cpp
@@ -25,4 +25,30 @@ class SkipNode{
 
 };
 
+class SkipList{
+    int MAX_LEVEL;
+    float P;
+    int level;
+    SkipNode *header;
+public;
+    SkipList(int max_lvl,float prob){
+        MAX_LEVEL = max_lvl;
+        P = prob;
+        level = 0;
+        heder = new SkipNode(INT_MIN,MAX_LEVEL);
+    }
 
+    ~SkipList(){
+        delete header;
+    }
+
+    int randomLevel(){
+        float r = (float)rand() / RAND_MAX;
+        itn lvl = 0;
+        while(r<P && lvl < MAX_LEVEL){
+            lvl++;
+            r = (float)rand() / RAND_MAX;
+        }
+        return lvl;
+    }
+}
