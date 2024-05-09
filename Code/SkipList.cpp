b5c8e19f3c942a49b317a44ee872480f534139dd
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2024-05-09 17:12:07 +0530

    Update SkipList.cpp

--- Code/SkipList.cpp
+++ Code/SkipList.cpp
@@ -3,7 +3,6 @@
 #include <ctime>
 #include <climits>
 #include <string>
-#include <limits>
 
 using namespace std;
 
@@ -155,13 +154,7 @@ int main() {
         cout << "\nEnter your choice: ";
         cin >> choice;
 
-           if (!(cin >> choice))
-        {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Invalid input. Please enter a number.\n";
-            continue; 
-        }
+         
         
         switch (choice) {
             case 1:
