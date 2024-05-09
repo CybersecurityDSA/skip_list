87d34d49c936a88cd7f47a7ffb775da9b0c605bd
Mahalaxmi2710 <167641516+Mahalaxmi2710@users.noreply.github.com>
2024-05-09 12:10:29 +0530

    Update Test_cases.cpp

---
+++ Test_Cases_for_SkipList/Test_cases.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <climits>
+#include <string>
+#include <cassert>
+#include "SkipList.h" 
+
+using namespace std;
+
+int main() {
+    srand((unsigned)time(0));
+    SkipList skipList(3, 0.5);
+
+    // Test insertion and search
+    skipList.insertElement("HELLO");
+    assert(skipList.searchElement("HELLO") == true);
+
+    skipList.insertElement("WORLD");
+    skipList.insertElement("APPLE");
+    assert(skipList.searchElement("WORLD") == true);
+    assert(skipList.searchElement("APPLE") == true);
+
+    assert(skipList.searchElement("FOOBAR") == false);
+
+    // Test insertion and search with lowercase and special characters
+    skipList.insertElement("hello");
+    skipList.insertElement("CYBER1234");
+    skipList.insertElement("268trie");
+    skipList.insertElement("HeLLo");
+    skipList.insertElement("@#$%^");
+    skipList.insertElement("!DSA");
+    skipList.insertElement("crypto$$");
+    skipList.insertElement("A");
+    skipList.insertElement("pneumonoultramicroscopicsilicovolcanoconiosis");
+    skipList.insertElement("HeLLo1234#");
+
+    assert(skipList.searchElement("hello") == true);
+    assert(skipList.searchElement("CYBER1234") == true);
+    assert(skipList.searchElement("268trie") == true);
+    assert(skipList.searchElement("HeLLo") == true);
+    assert(skipList.searchElement("@#$%^") == true);
+    assert(skipList.searchElement("!DSA") == true);
+    assert(skipList.searchElement("crypto$$") == true);
+    assert(skipList.searchElement("A") == true);
+    assert(skipList.searchElement("pneumonoultramicroscopicsilicovolcanoconiosis") == true);
+    assert(skipList.searchElement("HeLLo1234#") == true);
+    assert(skipList.searchElement("FoodIsImportant!!") == false);
+
+    // Test deletion
+    assert(skipList.deleteElement("hello") == true);
+    assert(skipList.deleteElement("@#$%^") == true);
+    assert(skipList.deleteElement("!DSA") == true);
+    assert(skipList.deleteElement("dsa!@") == false);
+
+    // Test edge case
+    assert(skipList.searchElement("Madness") == false);
+    assert(skipList.deleteElement("madNESS") == false);
+
+    // Test case sensitivity
+    skipList.insertElement("hello");
+    skipList.insertElement("WORLD");
+    skipList.insertElement("HeLLo");
+
+    assert(skipList.searchElement("hello") == true);
+    assert(skipList.searchElement("HeLLo") == true);
+    assert(skipList.searchElement("WORLD") == true);
+
+    return 0;
+}
